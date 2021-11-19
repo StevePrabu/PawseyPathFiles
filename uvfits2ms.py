4b9fc28f29eedb492ef64ea5d6a600170c06fe8a
steveprabu <steverajprabu@gmail.com>
2021-11-19 10:19:58 +0800

    now also does channel averaging to drop form 10kHz to 40kHz freq res

--- uvfits2ms.py
+++ uvfits2ms.py
@@ -4,7 +4,11 @@ from argparse import ArgumentParser
 def main(args):
 
     ## casa conversion task
-    importuvfits(fitsfile=args.uvfits, vis="{}.ms".format(args.obs))
+    importuvfits(fitsfile=args.uvfits, vis="{}native_res.ms".format(args.obs))
+
+    ## perform channel donw-sampling
+    mstransform(vis="{}native_res.ms".format(args.obs),outputvis="{}.ms".format(args.obs),chanaverage=True, chanbin=(4))
+    
 
 if __name__ == "__main__":
     parser = ArgumentParser("uvfits2ms", description="script to convert uvfits to ms format using CASA tasks")
